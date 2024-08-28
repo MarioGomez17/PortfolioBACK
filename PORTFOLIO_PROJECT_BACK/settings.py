cc8059c98680c32065cb386477dcf075e8c0a3f5
mariog44 <mg441236@gmail.com>
2024-08-28 09:51:08 -0500

    MODIFICACIÓN DE LA CONFIGURACIÓN. Se agregó la aplicación POSTFOLIO_APP en la sección de INSTALLED_APPS de la configuración, de la misma forma que se agrego la aplicación de REST FRAMEWORK.

--- PORTFOLIO_PROJECT_BACK/settings.py
+++ PORTFOLIO_PROJECT_BACK/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PORTFOLIO_APP',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
