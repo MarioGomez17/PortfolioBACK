1ef008ec1123adfb117681226c1f7bf3c73c698c
mariog44 <mg441236@gmail.com>
2024-08-28 18:24:15 -0500

    ACTUALIZACIÓN DE MODELOS. Se agregaron los campos de Logo a los Modelos de Lenguaje de Programación y Tecnología para guardar la URL de la imagen.

--- PORTFOLIO_APP/Models/TechnologyModel.py
+++ PORTFOLIO_APP/Models/TechnologyModel.py
@@ -7,6 +7,8 @@ class TechnologyModel(models.Model):
     Name_Technology = models.CharField(max_length=50, db_column='Name_Technology', default='', null=False)
     Division_Technology = models.ForeignKey(DivisionModel, on_delete=models.PROTECT, db_column='Division_Technology')
     ProgrammingLanguage_Technology = models.ForeignKey(ProgrammingLanguageModel, on_delete=models.PROTECT, db_column='ProgrammingLanguage_Technology')
+    Logo_Technology = models.TextField(db_column='Logo_Technology', default='', null=False)
+
 
     def __str__(self):
         return self.Name_Technology
