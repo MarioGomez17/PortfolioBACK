901577d8e34b250f673df29c523f8682feb76353
mariog44 <mg441236@gmail.com>
2024-09-02 15:15:17 -0500

    AJUSTE DE MODELOS. Se agregó el campo Descripción al modelo de Proyecto.

--- PORTFOLIO_APP/Models/ProjectModel.py
+++ PORTFOLIO_APP/Models/ProjectModel.py
@@ -4,6 +4,7 @@ from django.db import models
 class ProjectModel(models.Model):
     Id_Project = models.AutoField(primary_key=True, db_column='Id_Project', null=False)
     Name_Project = models.CharField(max_length=50, db_column='Name_Project', default='', null=False)
+    Description_Project = models.TextField(db_column='Description_Project', default='', null=False)
     URL_Project = models.URLField(db_column='URL_Project', default='', null=False)
     Technologies_Project = models.ManyToManyField('TechnologyModel')
 
