db2a5aa29ea30fbba216138b3dee801d0c283a7b
mariogomez441236 <mariogomez441236@gmail.com>
2024-10-22 20:47:03 -0500

    MODIFICACIÓN DE LOS MODELOS. Se modificaron los modelos para agregarle los nuevos campos correspondientes a los nuevos modelos.

--- PORTFOLIO_APP/Models/DeveloperModel.py
+++ PORTFOLIO_APP/Models/DeveloperModel.py
@@ -6,15 +6,16 @@ class DeveloperModel(models.Model):
     Name_Developer = models.CharField(max_length=50, db_column='Name_Developer', default='', null=False)
     LastName_Developer = models.CharField(max_length=50, db_column='LastName_Developer', default='', null=False)
     Email_Developer = models.EmailField(db_column='Email_Developer', default='', null=False)
-    Password_Developer = models.CharField(max_length=255, db_column='Password_Developer', default='', null=False)
     Phone_Developer = models.CharField(max_length=15, db_column='Phone_Developer', default='', null=False)
     LinkedinName_Developer = models.URLField(db_column='LinkedinName_Developer', default='', null=False)
     LinkedinUrl_Developer = models.URLField(db_column='LinkedinUrl_Developer', default='', null=False)
-    GitGubName_Developer = models.URLField(db_column='GitGubName_Developer', default='', null=False)
-    GitGubUrl_Developer = models.URLField(db_column='GitGubUrl_Developer', default='', null=False)
+    GitHubName_Developer = models.URLField(db_column='GitHubName_Developer', default='', null=False)
+    GitHubUrl_Developer = models.URLField(db_column='GitHubUrl_Developer', default='', null=False)
     City_Developer = models.ForeignKey(CityModel, on_delete=models.PROTECT, db_column='City_Developer')
     Description_Developer = models.TextField(db_column='Description_Developer', default='', null=False)
     Technologies_Developer = models.ManyToManyField('TechnologyModel')
+    Skills_Developer = models.ManyToManyField('SkillModel')
+    Languages_Developer = models.ManyToManyField('LanguageModel')
 
     def __str__(self):
         return self.Name_Developer
--- PORTFOLIO_APP/Models/TechnologyModel.py
+++ PORTFOLIO_APP/Models/TechnologyModel.py
@@ -1,11 +1,11 @@
 from django.db import models
-from .DivisionModel import DivisionModel
+from .SkillModel import SkillModel
 
 class TechnologyModel(models.Model):
     Id_Technology = models.AutoField(primary_key=True, db_column='Id_Technology', null=False)
     Name_Technology = models.CharField(max_length=50, db_column='Name_Technology', default='', null=False)
-    Division_Technology = models.ForeignKey(DivisionModel, on_delete=models.PROTECT, db_column='Division_Technology')
     Logo_Technology = models.TextField(db_column='Logo_Technology', default='', null=False)
+    Skill_Technology = models.ForeignKey(SkillModel, on_delete=models.PROTECT, db_column='Skill_Technology', default=1)
 
 
     def __str__(self):
