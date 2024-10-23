a895d3c74aff44276b37322a8afe24b04442f49b
mariogomez441236 <mariogomez441236@gmail.com>
2024-10-22 20:09:34 -0500

    ELIMINACIÓN DE ARCHIVOS. Se eliminaron los archivos correspondientes al modelo de Division.

--- PORTFOLIO_APP/Serializers/DivisionSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..Models import DivisionModel
-
-
-class DivisionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DivisionModel
-        fields = '__all__'
--- PORTFOLIO_APP/Views/DivisionViews/CreateDivision.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets, generics
-from ...Models.DivisionModel import DivisionModel
-from ...Serializers.DivisionSerializer import DivisionSerializer
-
-
-class DivisionCreateView(generics.CreateAPIView):
-    queryset = DivisionModel.objects.all()
-    serializer_class = DivisionSerializer
