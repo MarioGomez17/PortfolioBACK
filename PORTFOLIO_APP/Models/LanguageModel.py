91fa7e33d407aa878de15f28f50588c26d3f8a2d
mariogomez441236 <mariogomez441236@gmail.com>
2024-10-22 20:13:43 -0500

    ADICIÓN DE MODELOS. Se agregaron nuevos modelos para administrar la información sobre habilidades e idiomas que domina el desarrollador.

---
+++ PORTFOLIO_APP/Models/LanguageModel.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class LanguageModel(models.Model):
+    Id_Language = models.AutoField(primary_key=True, db_column='Id_Language', null=False)
+    Name_Language = models.CharField(max_length=50, db_column='Name_Language', default='', null=False)
+
+    def __str__(self):
+        return self.Name_Language
+
+    class Meta:
+        managed = True
+        db_table = 'Language'
