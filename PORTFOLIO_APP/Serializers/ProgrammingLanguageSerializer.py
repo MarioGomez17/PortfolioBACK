28059d46adcc1d4425d61a40957c0ff91cd2fd83
mariogomez441236 <mariogomez441236@gmail.com>
2024-11-04 23:42:28 -0500

    CAMBIO DE ESTRUCTURACIÓN DE LA INFORMACIÓN. Se cambió la forma en la que se relaciona y se serializa la información sobre los lenguajes de programación y sus tecnologías.

--- PORTFOLIO_APP/Serializers/ProgrammingLanguageSerializer.py
+++ PORTFOLIO_APP/Serializers/ProgrammingLanguageSerializer.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from ..Models import ProgrammingLanguageModel
+from .TechnologySerializer import TechnologySerializer
 
 
 class ProgrammingLanguageSerializer(serializers.ModelSerializer):
+    Technologies_ProgrammingLanguage = TechnologySerializer(read_only=True, many=True)
     class Meta:
         model = ProgrammingLanguageModel
-        fields = ['Id_ProgrammingLanguage', 'Name_ProgrammingLanguage', 'Logo_ProgrammingLanguage']
+        fields = ['Id_ProgrammingLanguage', 'Name_ProgrammingLanguage', 'Logo_ProgrammingLanguage', 'Technologies_ProgrammingLanguage']
--- PORTFOLIO_APP/Serializers/TechnologySerializer.py
+++ PORTFOLIO_APP/Serializers/TechnologySerializer.py
@@ -1,11 +1,9 @@
 from rest_framework import serializers
 from ..Models import TechnologyModel
 from .DivisionSerializer import DivisionSerializer
-from .ProgrammingLanguageSerializer import ProgrammingLanguageSerializer
 
 class TechnologySerializer(serializers.ModelSerializer):
     Division_Technology = DivisionSerializer(read_only=True)
-    ProgrammingLanguage_Technology = ProgrammingLanguageSerializer(read_only=True)
     class Meta:
         model = TechnologyModel
-        fields = ['Id_Technology', 'Name_Technology', 'Logo_Technology', 'Division_Technology', 'ProgrammingLanguage_Technology']
+        fields = ['Id_Technology', 'Name_Technology', 'Logo_Technology', 'Division_Technology']
