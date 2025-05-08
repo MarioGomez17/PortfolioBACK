972f96a52ab6111fd96d892df3d8103197628c6b
MarioGomez17 <mg441236@gmail.com>
2025-05-07 23:52:41 -0500

    UPDATE MODEL AND SERIALIZAER Technology Model and Serializer was changed to show the Programming Language parent.

--- PORTFOLIO_APP/Models/TechnologyModel.py
+++ PORTFOLIO_APP/Models/TechnologyModel.py
@@ -7,8 +7,8 @@ class TechnologyModel(models.Model):
     Name_Technology = models.CharField(max_length=50, db_column='Name_Technology', default='', null=False)
     Description_Technology = models.CharField(max_length=1000, db_column='Description_Technology', default='', null=False)
     Logo_Technology = models.TextField(db_column='Logo_Technology', default='', null=False)
-    ProgrammingLanguage_Technology = models.ForeignKey(ProgrammingLanguageModel, on_delete=models.PROTECT, db_column='ProgrammingLanguage_Technology', default=1, related_name='Technologies_ProgrammingLanguage')
-    Division_Technology = models.ForeignKey(DivisionModel, on_delete=models.PROTECT, db_column='Division_Technology', default=1)
+    ProgrammingLanguage_Technology = models.ForeignKey(ProgrammingLanguageModel, on_delete=models.PROTECT, db_column='ProgrammingLanguage_Technology', default=1, related_name='Technologies_ProgrammingLanguage', null=True)
+    Division_Technology = models.ForeignKey(DivisionModel, on_delete=models.PROTECT, db_column='Division_Technology', default=1, null=True)
     
 
     def __str__(self):
--- PORTFOLIO_APP/Serializers/DeveloperSerializer.py
+++ PORTFOLIO_APP/Serializers/DeveloperSerializer.py
@@ -7,6 +7,7 @@ from .LanguageSerializer import LanguageSerializer
 from .EducationSerializer import EducationSerializer
 from .SkillSerializer import SkillSerializer
 from .ProjectSerializer import ProjectSerializer
+from .ExperienceSerializer import ExperienceSerializer
 
 
 class DeveloperSerializer(serializers.ModelSerializer):
@@ -17,7 +18,8 @@ class DeveloperSerializer(serializers.ModelSerializer):
     Education_Developer = EducationSerializer(many=True, read_only=True)
     Skills_Developer = SkillSerializer(many=True, read_only=True)
     Projects_Developer = ProjectSerializer(many=True, read_only=True)
+    Experience_Developer = ExperienceSerializer(many=True, read_only=True)
 
     class Meta:
         model = DeveloperModel
-        fields = ['Id_Developer', 'Name_Developer', 'LastName_Developer', 'Email_Developer', 'Phone_Developer', 'LinkedinName_Developer', 'LinkedinUrl_Developer', 'GitHubName_Developer', 'GitHubUrl_Developer', 'City_Developer', 'Description_Developer', 'ProgrammingLanguages_Developer', 'Technologies_Developer', 'Languages_Developer', 'Education_Developer', 'Skills_Developer', 'Projects_Developer']
+        fields = ['Id_Developer', 'Name_Developer', 'LastName_Developer', 'Email_Developer', 'Phone_Developer', 'LinkedinName_Developer', 'LinkedinUrl_Developer', 'GitHubName_Developer', 'GitHubUrl_Developer','City_Developer', 'Description_Developer', 'ProgrammingLanguages_Developer', 'Technologies_Developer', 'Languages_Developer', 'Education_Developer', 'Skills_Developer', 'Projects_Developer', 'Experience_Developer']
