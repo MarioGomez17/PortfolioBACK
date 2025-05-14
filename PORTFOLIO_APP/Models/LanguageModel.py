0513e5446bef711be3874bf43a5f31c12f3cbcbf
MarioGomez17 <mg441236@gmail.com>
2025-05-13 22:46:41 -0500

    UPDATE OLD MODELS Old models was updated to fit the new models

--- PORTFOLIO_APP/Models/LanguageModel.py
+++ PORTFOLIO_APP/Models/LanguageModel.py
@@ -4,6 +4,7 @@ from django.db import models
 class LanguageModel(models.Model):
     Id_Language = models.AutoField(primary_key=True, db_column='Id_Language', null=False)
     Name_Language = models.CharField(max_length=50, db_column='Name_Language', default='', null=False)
+    Level_Language = models.CharField(max_length=50, db_column='Level_Language', default='', null=False)
 
     def __str__(self):
         return self.Name_Language
